a4085567d4668d1a27f585e8bfb180fbfee96beb
rodrifernandez094 <rodrifernandez094@gmail.com>
2022-03-04 14:29:05 -0300

    add useEffect cleanup function

--- components/AuthStateChanged.js
+++ components/AuthStateChanged.js
@@ -1,17 +1,28 @@
 import useAuth from "../context/authContext";
 import AuthService from "../services/AuthService";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Spinner from "./Spinner";
 
 export default function AuthStateChanged({ children }) {
-  const { setUser } = useAuth();
+  const { setUser, user } = useAuth();
   const [loading, setLoading] = useState(true);
+  const Router = useRouter();
 
   useEffect(() => {
-    AuthService.authStateChanged((credentials) => {
-      setUser(credentials);
-      setLoading(false);
+    // const unsubscribeSnapshot;
+    const unsubscribeAuth = AuthService.authStateChanged((credentials) => {
+      if (user) {
+        Router.push("/login");
+      } else {
+        setUser(credentials);
+        setLoading(false);
+      }
     });
+
+    return () => {
+      unsubscribeAuth();
+    };
   }, [setUser]);
 
   if (loading) {
