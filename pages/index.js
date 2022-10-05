cf6483a5c3f041d7609603b6cf817e7370f4f956
rodrifernandez094 <rodrifernandez094@gmail.com>
2022-10-05 17:58:16 -0300

    Change img tag for picture. Use webp image

--- pages/index.js
+++ pages/index.js
@@ -56,10 +56,12 @@ const Home = () => {
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <source srcset="/crypto.webp" type="image/webp" />
+        <source srcset="/crypto.jpg" type="image/jpeg" />
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="/crypto.jpg"
-          alt="crypto"
+          src="/crypto.webp"
+          alt="crypto currency background"
         />
       </div>
     </div>
