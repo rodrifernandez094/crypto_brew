5b7297cf7e3ab20193a3a6f7251612a98d761da2
rodrifernandez094 <rodrifernandez094@gmail.com>
2022-10-06 21:01:05 -0300

    Fix typo on source tag. Change div for picture tag

--- pages/index.js
+++ pages/index.js
@@ -55,15 +55,15 @@ const Home = () => {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <source srcset="/crypto.webp" type="image/webp" />
-        <source srcset="/crypto.jpg" type="image/jpeg" />
+      <picture className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <source srcSet="/crypto.webp" type="image/webp" />
+        <source srcSet="/crypto.jpg" type="image/jpeg" />
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="/crypto.webp"
           alt="crypto currency background"
         />
-      </div>
+      </picture>
     </div>
   );
 };
